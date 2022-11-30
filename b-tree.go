34ee2a39428c33e0b2226b913dd5bf7f98e13868
Chris Allum <ctallum@gmail.com>
2022-11-29 20:58:24 -0500

    Fix Insert with pointers

--- b-tree.go
+++ b-tree.go
@@ -8,7 +8,7 @@ import (
 type Cell_BTree struct {
 	ID       int
 	cur_size int
-	keys     []int
+	keys     []*int
 	children []*Cell_BTree
 	parent   *Cell_BTree
 }
@@ -25,7 +25,7 @@ type Value_Location struct {
 }
 
 func NewCell_BTree(size int, ID int) *Cell_BTree {
-	keys := make([]int, size)
+	keys := make([]*int, size)
 	children := make([]*Cell_BTree, size+1)
 	cell := &Cell_BTree{ID, 0, keys, children, nil}
 	return cell
@@ -37,11 +37,10 @@ func NewSet_BTree(degree int) *Set_BTree {
 }
 
 func (s *Set_BTree) insert(v int) {
-
 	// Step 0, if the tree is empty, initialize it
 	if s.root == nil {
 		new_node := NewCell_BTree(s.degree, 0)
-		new_node.keys[0] = v
+		new_node.keys[0] = &v
 		new_node.cur_size += 1
 		s.root = new_node
 		return
@@ -54,15 +53,20 @@ func (s *Set_BTree) insert(v int) {
 		if c.IsLeaf() {
 			return c
 		}
+		
+
+	
 
 		// find first key that is smaller than or equal to v
 		search_idx := 0
-		for search_idx < c.cur_size && v > c.keys[search_idx] {
+		for search_idx < c.cur_size && v > *c.keys[search_idx] {
 			search_idx += 1
 		}
+		
+		
 
 		// value is already in tree, return to that cell
-		if c.keys[search_idx] == v {
+		if c.keys[search_idx] != nil && *c.keys[search_idx] == v {
 			return c
 		}
 
@@ -73,29 +77,31 @@ func (s *Set_BTree) insert(v int) {
 	// Step 2) Insert value into leaf node keys
 	insert_node := Find_Leaf(s.root, v)
 
+
 	
 
 	// check if value is already in tree
 	if insert_node.Contains(v) {
 		return
 	}
-	
-	
 
-	insert_node.keys[insert_node.cur_size] = v
+
+	insert_node.keys[insert_node.cur_size] = &v
 	insert_node.cur_size += 1
-
 	
 	// Step 3) Sort list
 	PartialSort(insert_node.keys, insert_node.cur_size)
-	fmt.Println("HI")
-	s.print()
-	fmt.Println("HI")
+
+
 	// Step 4) Check if we are done
 	if insert_node.cur_size != s.degree {
 		return
 	}
 
+	if v == 2081{
+		fmt.Println(the_dereferencer(insert_node.keys))
+	}
+	
 	
 	var FixTreeUpwards func(s *Set_BTree, c *Cell_BTree)
 	FixTreeUpwards = func(s *Set_BTree, c *Cell_BTree) {
@@ -189,12 +195,12 @@ func (s *Set_BTree) search(v int) *Value_Location {
 
 		// find first key that is smaller than or equal to v
 		search_idx := 0
-		for search_idx < c.cur_size && v > c.keys[search_idx] {
+		for search_idx < c.cur_size && v > *c.keys[search_idx] {
 			search_idx += 1
 		}
 
 		// if that key is equal to v, value has been found
-		if c.keys[search_idx] == v {
+		if c.keys[search_idx] != nil && *c.keys[search_idx] == v {
 			return &Value_Location{c, search_idx, v}
 		}
 
@@ -239,7 +245,7 @@ func (s *Set_BTree) min() *Value_Location {
 	for current_node.children[0] != nil {
 		current_node = current_node.children[0]
 	}
-	return &Value_Location{current_node, 0, current_node.keys[0]}
+	return &Value_Location{current_node, 0, *current_node.keys[0]}
 }
 
 func (s *Set_BTree) max() *Value_Location {
@@ -251,7 +257,7 @@ func (s *Set_BTree) max() *Value_Location {
 	for current_node.children[0] != nil {
 		current_node = current_node.children[current_node.cur_size]
 	}
-	return &Value_Location{current_node, 0, current_node.keys[current_node.cur_size-1]}
+	return &Value_Location{current_node, 0, *current_node.keys[current_node.cur_size-1]}
 }
 
 func test_BTree() {
@@ -260,7 +266,7 @@ func test_BTree() {
 	// First, insert all of 1, 2, 3, ..., 100 in order into an empty B-tree.
 	// fmt.Println("***** INSERTING RIGHT **************************************************")
 	s := NewSet_BTree(size)
-	for i := -7; i <= 0; i += 1 {
+	for i := -1000; i <= 1000; i += 1 {
 		fmt.Printf("Inserting %d\n", i)
 		s.insert(i)
 		s.print()
@@ -292,12 +298,12 @@ func main() {
 
 // ************** HELPER CODE **********************************
 
-func PartialSort(arr []int, partitian int) {
-	var msort func(arr []int, i int, j int, temp []int)
-	var merge func(arr []int, i int, k int, j int, temp []int)
+func PartialSort(arr []*int, partitian int) {
+	var msort func(arr []*int, i int, j int, temp []int)
+	var merge func(arr []*int, i int, k int, j int, temp []int)
 
 	// merge sort definition
-	msort = func(arr []int, i int, j int, temp []int) {
+	msort = func(arr []*int, i int, j int, temp []int) {
 		if i == j {
 			return
 		}
@@ -308,28 +314,28 @@ func PartialSort(arr []int, partitian int) {
 	}
 
 	// merge definition
-	merge = func(arr []int, i int, k int, j int, temp []int) {
+	merge = func(arr []*int, i int, k int, j int, temp []int) {
 		first_end := k
 		second_end := j
 		var largest int
 		for idx := j; idx >= i; idx-- {
 			if first_end == i-1 {
-				largest = arr[second_end]
+				largest = *arr[second_end]
 				second_end -= 1
 			} else if second_end == k {
-				largest = arr[first_end]
+				largest = *arr[first_end]
 				first_end -= 1
-			} else if arr[second_end] > arr[first_end] {
-				largest = arr[second_end]
+			} else if *arr[second_end] > *arr[first_end] {
+				largest = *arr[second_end]
 				second_end -= 1
 			} else {
-				largest = arr[first_end]
+				largest = *arr[first_end]
 				first_end -= 1
 			}
 			temp[idx] = largest
 		}
 		for idx2 := j; idx2 >= i; idx2-- {
-			arr[idx2] = temp[idx2]
+			*arr[idx2] = temp[idx2]
 		}
 	}
 
@@ -353,11 +359,11 @@ func (s *Set_BTree) print() {
 			// print current self
 			//fmt.Printf("%s%d : %d : %d\n", prefix, c.keys, c.cur_size, c.ID)
 			if signal && c.parent != nil {
-				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"/", c.keys[0:c.cur_size])
+				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"/", the_dereferencer(c.keys[0:c.cur_size]))
 			} else if !signal && c.parent != nil {
-				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"\\", c.keys[0:c.cur_size])
+				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"\\", the_dereferencer(c.keys[0:c.cur_size]))
 			} else {
-				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"-", c.keys[0:c.cur_size])
+				ascii_tree += fmt.Sprintf("%s%d\n", prefix+"-", the_dereferencer(c.keys[0:c.cur_size]))
 			}
 			//ascii_tree += fmt.Sprintf("%s%d\n", prefix, c.keys[0:c.cur_size])
 			// print left hafl of children
@@ -392,7 +398,7 @@ func (s *Set_BTree) print() {
 
 func (c *Cell_BTree) Contains(v int) bool {
 	for _, val := range c.keys[0:c.cur_size] {
-		if val == v {
+		if *val == v {
 			return true
 		}
 	}
@@ -411,7 +417,7 @@ func (c *Cell_BTree) ShiftCellItems(free_idx int) {
 	}
 
 	// clear out the key and the sourounding children the border it
-	c.keys[free_idx] = 0
+	c.keys[free_idx] = nil
 	c.children[free_idx] = nil
 	c.children[free_idx+1] = nil
 }
@@ -462,7 +468,7 @@ func (s *Set_BTree) DeleteFromLNonLeaf(loc *Value_Location) {
 	c.keys[idx] = swap_value
 
 	// remove the key from the leaf
-	child.keys[child.cur_size-1] = 0
+	child.keys[child.cur_size-1] = nil
 	child.cur_size -= 1
 
 	// check if we need to fix the leaf if it is too small
@@ -537,7 +543,7 @@ func (c *Cell_BTree) BorrowFromLeft() {
 
 	// remove last value from left
 	last_val := left_cell.keys[left_cell.cur_size-1]
-	left_cell.keys[left_cell.cur_size-1] = 0
+	left_cell.keys[left_cell.cur_size-1] = nil
 
 	// remove last child from left
 	last_child := left_cell.children[left_cell.cur_size]
@@ -582,7 +588,7 @@ func (c *Cell_BTree) BorrowFromRight() {
 
 	// remove first value from right
 	right_val := right_cell.keys[0]
-	right_cell.keys[0] = 0
+	right_cell.keys[0] = nil
 
 	// remove first key from right
 	right_child := right_cell.children[0]
@@ -678,3 +684,15 @@ func (s *Set_BTree) FixRoot(c *Cell_BTree) {
 	s.root = c.children[0]
 }
 
+func the_dereferencer(c []*int) []int{
+	new_array := make([]int, len(c))
+	for idx, _ := range c{
+		if c[idx] == nil{
+			new_array[idx] = 0
+		} else{
+			new_array[idx] = *c[idx]
+		}
+		
+	}
+	return new_array
+}
--- b-tree_test.go
+++ b-tree_test.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-
-	//"sort"
 	"testing"
 )
 
@@ -13,7 +11,7 @@ func GetRandom_BTree(degree int, n int) (*Set_BTree, []int) {
 
 	vals := make([]int, n)
 	for i := 0; i < n; i++ {
-		v := rand.Intn(1000)
+		v := rand.Intn(100000)
 		if v%2 == 0 {
 			// test negative values!
 			v = -v
@@ -26,44 +24,69 @@ func GetRandom_BTree(degree int, n int) (*Set_BTree, []int) {
 }
 
 func TestInsert_BTree(t *testing.T) {
-	for degree := 4; degree < 10; degree++ {
-		fmt.Println(degree)
+	for degree := 3; degree < 10; degree++ {
+
 		s, val := GetRandom_BTree(degree, 10*degree*degree)
-		s.print()
-		fmt.Println(val)
-		// for _, v := range val {
-		// 	location := s.search(v)
-		// 	if location == nil {
-		// 		t.Errorf("Error trying to find value.")
-		// 		fmt.Printf("Tried to find %d. Found nil\n", v)
-		// 	}
-		// 	if location.value != location.cell.keys[location.key_idx] {
-		// 		t.Errorf("Incorrect location read when trying to find value.")
-		// 		fmt.Printf("location value is %d, cell value is %d\n", location.value, location.cell.keys[location.key_idx])
-		// 	}
-		// 	if location.value != v {
-		// 		t.Error("Error trying to find value.")
-		// 		fmt.Printf("Expected %d, found %d\n", v, location.value)
-		// 	}
-		// }
+
+		for _, v := range val {
+			location := s.search(v)
+			if location == nil {
+				t.Errorf("Error trying to find value.")
+				fmt.Printf("Tried to find %d. Found nil\n", v)
+			}
+			if location.value != *location.cell.keys[location.key_idx] {
+				t.Errorf("Incorrect location read when trying to find value.")
+				fmt.Printf("location value is %d, cell value is %d\n", location.value, location.cell.keys[location.key_idx])
+			}
+			if location.value != v {
+				t.Error("Error trying to find value.")
+				fmt.Printf("Expected %d, found %d\n", v, location.value)
+			}
+		}
 
 	}
 
 }
 
-// func TestDelete_BTree(t *testing.T) {
-// 	for degree := 3; degree < 10; degree++ {
-// 		s, val := GetRandom_BTree(degree, 10*degree*degree)
+func TestDelete_BTree(t *testing.T) {
+	for degree := 3; degree < 10; degree++ {
+		s, val := GetRandom_BTree(degree, 10*degree*degree)
 
-// 		for _, v := range val {
-// 			s.delete(v)
+		for _, v := range val {
+			s.delete(v)
 
-// 			location := s.search(v)
+			location := s.search(v)
 
-// 			if location != nil {
-// 				t.Errorf("Error, value was not deleted.")
-// 				fmt.Printf("Tried to delete %d, but value was still found\n", v)
-// 			}
-// 		}
-// 	}
-// }
+			if location != nil {
+				t.Errorf("Error, value was not deleted.")
+				fmt.Printf("Tried to delete %d, but value was still found\n", v)
+			}
+		}
+	}
+}
+
+func TestSet_BTree_insert(t *testing.T) {
+	type fields struct {
+		degree int
+		root   *Cell_BTree
+	}
+	type args struct {
+		v int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Set_BTree{
+				degree: tt.fields.degree,
+				root:   tt.fields.root,
+			}
+			s.insert(tt.args.v)
+		})
+	}
+}
